84e5d74311a9ffd5b84e940a5eb2455273022333
anickacodes <anickalewis@pursuit.org>
2023-08-11 12:26:32 -0400

    added env

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
+  console.log("One of Our Environment Variables: ", import.meta.env.VITE_10_1_VAR)
 
   return (
     <>
     <Router >
 
       <h1>ScreenViews</h1>
+
+      <h2>-TEST- {import .meta. env.VITE_10_1_VAR} </h2>
       </Router>
     </>
   )
