344eb9ca83b0f5f5eed877e4b5d76e50bd61b686
anickacodes <anickalewis@pursuit.org>
2023-08-11 11:54:54 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import  {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Router >
+
+      <h1>ScreenViews</h1>
+      </Router>
+    </>
+  )
+}
+
+export default App
